bea0ce4ead45804b78fc42bd4668b428a2d43111
stefanoreddd <stefanogg95@gmail.com>
2023-04-05 17:10:47 +0200

    function log fixed.

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -17,26 +17,26 @@ let totalNumber = userNumber + cpuNumber;
 function evenOddGame(){
 
     if(userChoice == "pari" && totalNumber % 2 == 0){
-        console.log("Numero utente: " + userNumber)
-        console.log("Numero Computer: " + cpuNumber)
+        console.log("Numero utente: " + userNumber);
+        console.log("Numero Computer: " + cpuNumber);
         console.log("Totale:" + totalNumber);
         console.log("Il numero è pari! Vince il giocatore!");
     }
     else if(userChoice == "dispari" && totalNumber % 2 !== 0){
-        console.log("Numero utente: " + userNumber)
-        console.log("Numero Computer: " + cpuNumber)
+        console.log("Numero utente: " + userNumber);
+        console.log("Numero Computer: " + cpuNumber);
         console.log("Totale: " + totalNumber);
         console.log("Il numero è dispari! Vince il giocatore!");
     }
     else if(userChoice == "pari" && totalNumber % 2 !== 0){
-        console.log("Numero utente: " + userNumber)
-        console.log("Numero Computer: " + cpuNumber)
+        console.log("Numero utente: " + userNumber);
+        console.log("Numero Computer: " + cpuNumber);
         console.log("Totale: " + totalNumber);
         console.log("Il numero è dispari! Vince il computer!");
     } 
     else if(userChoice == "dispari" && totalNumber % 2 == 0){
-        console.log("Numero utente" + userNumber);
-        console.log("Numero Computer" + cpuNumber);
+        console.log("Numero utente: " + userNumber);
+        console.log("Numero Computer: " + cpuNumber);
         console.log("Totale: " + totalNumber);
         console.log("Il numero è pari! Vince il computer!");
     }
